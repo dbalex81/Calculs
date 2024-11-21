75cc651f771374a4bbe2e3f2b8bd293789b8bb97
alexi <alexi@DESKTOP-83MA21N>
2024-11-21 23:46:18 +0100

    Ajoutez des fichiers projet.

---
+++ Calculs/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+
+namespace Calculs
+{
+    /// <summary>
+    /// Application Calculs : addition ou multiplication de 2 nombres
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // variables 
+            Random rand = new Random(); // outil de génération de nombre aléatoire
+            int val1, val2; // mémorisation de nombres aléatoires
+            int solution; // calcul de la solution
+            int reponse = 0; // saisie de la réponse de l'utilisateur
+            string choix; // saisie du choix de l'utilsiateur
+            bool correct;
+            
+
+            // boucle sur le menu
+            choix = "1";
+            while (choix != "0")
+            {
+                // affiche le menu et saisi le choix
+                Console.WriteLine("Addition ....................... 1");
+                Console.WriteLine("Multiplication ................. 2");
+                Console.WriteLine("Quitter ........................ 0");
+                Console.Write("Choix :                          ");
+                choix = Console.ReadLine();
+
+                // traitement des choix
+                switch (choix)
+                {
+                    case "1":
+                        // addition
+                        val1 = rand.Next(1, 10);
+                        val2 = rand.Next(1, 10);
+                        correct = false;
+                        // saisie de la réponse
+                        while (!correct)
+                        {
+                            try
+                            {
+                                Console.Write(val1 + " + " + val2 + " = ");
+                                reponse = int.Parse(Console.ReadLine());
+                                correct = true;
+                            }
+                            catch
+                            {
+                                Console.WriteLine("Entrer un nombre entier ");
+                            }
+                        }                       
+                        // comparaison avec la bonne réponse
+                        solution = val1 + val2;
+                        if (reponse == solution)
+                        {
+                            Console.WriteLine("Bravo !");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Faux : " + val1 + " + " + val2 + " = " + solution);
+                        }
+                        break;
+                    case "2":
+                        // choix de la multiplication
+                        val1 = rand.Next(1, 10);
+                        val2 = rand.Next(1, 10);
+                        correct = false;
+                        // saisie de la réponse
+                        while (!correct)
+                        {
+                            try
+                            {
+                                Console.Write(val1 + " x " + val2 + " = ");
+                                reponse = int.Parse(Console.ReadLine());
+                                correct = true;
+                            }
+                            catch
+                            {
+                                Console.WriteLine("Entrer un nombre entier");
+                            }
+                        }
+                        
+                        // comparaison avec la bonne réponse
+                        solution = val1 * val2;
+                        if (reponse == solution)
+                        {
+                            Console.WriteLine("Bravo !");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Faux : " + val1 + " x " + val2 + " = " + solution);
+                        }
+                        break;
+                    case "0": //demande de fin de programme
+                        break;
+                    default:
+                        Console.WriteLine("Erreur de saisie");
+                        break;                       
+                }
+            }
+        }
+    }
+}
+
